608c5cf330cf29e45a11a9f9c458fe9b1405d312
imran sangle <imransangle91@gmail.com>
2024-12-08 20:47:48 +0530

    Script files modifications.
    Script files are modified to run only in linux and msys2(for windows) shell

--- buildDebug.bat
+++
@@ -1,3 +0,0 @@
-@echo making buildfiles with debug symbols and logs enable..
-cmake -S . -B build -G"Unix Makefiles" -D CMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug
-@echo buildfiles generated successfully.
